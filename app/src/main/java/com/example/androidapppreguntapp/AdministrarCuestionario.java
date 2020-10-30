618d5f20c522e788903914c54aee70452018e82c
Leyton <50716739+Bioleyton888@users.noreply.github.com>
2020-10-29 21:53:45 -0300

    Ahora el nombre y apellido se buscan en la base de datos.....deal with it

--- app/src/main/java/com/example/androidapppreguntapp/AdministrarCuestionario.java
+++ app/src/main/java/com/example/androidapppreguntapp/AdministrarCuestionario.java
@@ -1,4 +1,4 @@
-package com.example.androidapppreguntapp;
+ package com.example.androidapppreguntapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
--- app/src/main/java/com/example/androidapppreguntapp/MenuPrincipalAdministrador.java
+++ app/src/main/java/com/example/androidapppreguntapp/MenuPrincipalAdministrador.java
@@ -7,11 +7,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class MenuPrincipalAdministrador extends AppCompatActivity {
+    funciones_varias xamp = new funciones_varias();
     TextView tvBienvenida;
     Button botonPerfil,botonCerrarSesion,BotonAdministrarEncuestas;
     String nombre, apellido;
+    RequestQueue requestQueue;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +34,8 @@ public class MenuPrincipalAdministrador extends AppCompatActivity {
         botonCerrarSesion = (Button)findViewById(R.id.botonVolver);
         BotonAdministrarEncuestas= (Button)findViewById(R.id.botonirAAdministrarEncuestas);
 
-        nombre = getIntent().getStringExtra("Nombre").toLowerCase();
-        apellido = getIntent().getStringExtra("Apellido").toLowerCase();
+        buscarNombreyApellido("http://"+ xamp.ipv4()+":"+ xamp.port()+"/webservicesPreguntAPP/buscar_NombreYApellidos.php?per_correo="+getIntent().getStringExtra("correo")+"");
 
-        //esto es para hacer la primera letra mayuscula
-        tvBienvenida.setText("¡Hola "+nombre.substring(0,1).toUpperCase()+nombre.substring(1) +" "+apellido.substring(0,1).toUpperCase()+apellido.substring(1)+"!");
 
 
         botonCerrarSesion.setOnClickListener(new View.OnClickListener() {
@@ -57,6 +67,41 @@ public class MenuPrincipalAdministrador extends AppCompatActivity {
         startActivity(intent);
         finish();
     }
+    private void buscarNombreyApellido(String rutaWebServices){
+
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(rutaWebServices, new Response.Listener<JSONArray>() {
+            @Override
+            public void onResponse(JSONArray response) {
+                JSONObject jsonObject = null;
+                for (int i = 0; i < response.length(); i++) {
+                    try {
+                        jsonObject = response.getJSONObject(i);
+                        nombre= jsonObject.getString("per_nombre");
+                        apellido= jsonObject.getString("per_apellidos");
+                        tvBienvenida.setText("¡Hola "+nombre.substring(0,1).toUpperCase()+nombre.substring(1) +" "+apellido.substring(0,1).toUpperCase()+apellido.substring(1)+"!");
+
+
+
+        //irAMenuPrincipalAdministrador(jsonObject.getString("per_nombre"), jsonObject.getString("per_apellidos"), jsonObject.getString("per_correo"));
+                    } catch (JSONException e) {
+                        Toast.makeText(getApplicationContext(), e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+
+
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getApplicationContext(),error.toString(),Toast.LENGTH_SHORT).show();
+            }
+        }
+        );
+        requestQueue= Volley.newRequestQueue(this);
+        requestQueue.add(jsonArrayRequest);
+    }
+
 }
 
 
